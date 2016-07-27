3c8f35197d132d5e0abc804768319e58d29b1a6b
ondreian <benjamin.clos@gmail.com>
2016-07-27 16:47:31 -0400

    deployment

--- app/utils/onlyEvery.js
+++ app/utils/onlyEvery.js
@@ -1,7 +1,10 @@
 export default function onlyEvery (wait, fn) {
   let running = false
   return function ratelimited (...args) {
-    if (running) return
+    if (running) {
+      console.log("skipping ratelimited action...")
+      return
+    }
     running = setTimeout( ()=> {
       running = false
       return fn.apply(this, args)
--- app/views/Dashboard.js
+++ app/views/Dashboard.js
@@ -1,6 +1,9 @@
-import m          from "mithril"
-import App        from "layouts/App"
-import {bulma, pipe, styles}    from "utils/bulma"
+import m                      from "mithril"
+import lunr                   from "lunr"
+import App                    from "layouts/App"
+import {bulma, pipe, styles}  from "utils/bulma"
+import onlyEvery              from "utils/onlyEvery"
+
 
 const LEVEL_RANGE = /^([0-9]+)(|\s+)(-|:|between)(|\s+)([0-9]+)$/
 
@@ -18,9 +21,10 @@ export default class Dashboard {
   }
 
   static leftPane (ctrl) {
-    const creatureList = m(`ol${styles.creatures()}`, ctrl.list()
-      .sort( (a,b)=> b.level - a.level )
-      .map(Dashboard.creatureItem(ctrl)))
+    const creatureList = m(`ol${styles.creatures()}`
+      , Dashboard.creatureItem(ctrl)
+          | ctrl.list().map()
+    )
       
     return bulma.leftPane([
         Dashboard.searchBar(ctrl)
@@ -31,7 +35,11 @@ export default class Dashboard {
   static searchBar (ctrl) {
     return bulma.searchBar(
         m("input[type=search]", {
-        oninput: (evt) => ctrl.search(evt)
+            oninput: onlyEvery(250, (evt) => ctrl.search(evt))
+          , config : (ele, isInitialized, ctx) => {
+              if (isInitialized) return true
+              ctx.retain = true
+            }
         })
       , m(`ol${styles.filters()}`, [
         m(`li.undead-filter${ ctrl.filters.undead ? styles.filterOn() : styles.filterOff() }`, {
@@ -50,7 +58,7 @@ export default class Dashboard {
 
   static creatureItem (ctrl) { return (creature) => {
       return m("li"
-        , { onclick: (evt)=> ctrl.active(creature) }
+        , { onclick: (evt)=> ctrl.active(creature), id: creature.name }
         , [
               m(`span${styles.level()}`, creature.level)
             , m("span", creature.name)
@@ -63,12 +71,15 @@ export default class Dashboard {
 
     const content = !creature
       ? m("h1", "please select a monster")
-      : [
+      : bulma.content([
             m("h1.name", creature.name)
           , m("h2.level", creature.level)
-          , m("h3.habitat", creature.habitat)
+          , m("h3.habitats", creature.habitats)
           , m("p.description", creature.description)
-        ]
+          , m(`ol${styles.rooms()}`,  (creature.rooms || []).filter( room => room ).map( room => {
+              return m(`li`, room)
+          }))
+        ])
 
     return content
       | pipe.view()
@@ -80,29 +91,62 @@ export default class Dashboard {
     this.creatures = m.prop([])
     this.filters   = {}
     this.active    = m.prop(false)
+    this.index     = lunr(function () {
+      this.field('name', { boost: 10 })
+      this.field('tags', { boost: 20 })
+      this.field('description')
+      this.ref('name')
+    })
     
     this.list      = ()=> {
-      return Object
+      const list = Object
         .keys(this.filters)
         .map( filter => this.filters[filter] )
         .reduce( (creatures, filter)=> creatures.filter(filter) , this.creatures())
+
+      if (!this.sorter) return list
+
+      return list.sort(this.sorter)
     }
 
-    m.request({method : "GET", url: "https://rawgit.com/ondreian/gemstone_data_project/master/creatures.json"}).then(this.creatures)
+    m
+      .request({method : "GET", url: "https://rawgit.com/ondreian/gemstone_data_project/master/creatures.json"})
+      .then(this.creatures)
+      .then( creatures => {
+        creatures.forEach( creature => this.index.add(creature) )
+      })
   }
 
   search (evt) {
+    delete this.filters.lunr
+    delete this.sorter
+
+    if (evt.target.value == "") {
+      delete this.filters.lunr
+      delete this.filters.level
+    }
+
     if (evt.target.value.match(LEVEL_RANGE)) {
       this.filters.level = Dashboard.level(evt.target.value)
       return
     }
 
     delete this.filters.level
-    // TODO: lunr search
+
+    const lookup = this.index.search(evt.target.value)
+    .filter( match => match.score > .05 )
+    .reduce( (lookup, match)=> {
+      lookup[match.ref] = match.score
+      return lookup
+    }, {})
+
+    this.filters.lunr = creature => lookup[creature.name]
+    this.sorter       = (a, b)   => lookup[b.name] - lookup[a.name]
+
   }
 
   static undead (creature) {
-    return creature.undead
+    return ~creature.tags.indexOf("undead")
   }
 
   static level (str) {
@@ -110,6 +154,5 @@ export default class Dashboard {
     return creature => creature.level <= max && creature.level >= min
     
   }
-
 
 }
